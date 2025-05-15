9b705ac9cfea93fc86d1ec2bab4dd242a7682631
khanlazyanas <anaskhan995620@gmail.com>
2025-05-15 22:13:34 +0530

    update cors in server.js

--- server.js
+++ server.js
@@ -17,12 +17,15 @@ dotenv.config();
 const app = express();
 
 // CORS & Cookie Parser
+import cors from "cors";
+
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: process.env.FRONTEND_URL || "http://localhost:5173",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
 }));
+
 
 app.use(cookieParser());
 
