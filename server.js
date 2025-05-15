e9470ece47621262de61e5e7ac10e0247a910202
khanlazyanas <anaskhan995620@gmail.com>
2025-05-15 12:26:39 +0530

    update server.js for latest changes

--- server.js
+++ server.js
@@ -17,7 +17,13 @@ dotenv.config();
 const app = express();
 
 // CORS & Cookie Parser
-app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+}));
+
 app.use(cookieParser());
 
 // ✅ Special middleware for Razorpay webhook route — must be raw
